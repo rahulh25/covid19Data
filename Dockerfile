46f6c76a132dc871506744934bf61ecefb463d8c
unknown <handoo.r@northeastern.edu>
2020-05-15 19:33:04 -0400

    Adding circle ci and new nginx config

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,5 @@
 FROM nginx
+RUN rm -rf /usr/share/nginx/html/*
 COPY build /usr/share/nginx/html
+COPY ./.nginx/nginx.conf /etc/nginx/nginx.conf
+ENTRYPOINT ["nginx", "-g", "daemon off;"]
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders lglobal data header', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/GLOBAL DATA/);
   expect(linkElement).toBeInTheDocument();
 });
